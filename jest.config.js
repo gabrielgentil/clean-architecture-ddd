5f23d3d27701a965df587ea31c2dccca896c5c2d
Gabriel Gentil <gabriel.gentil@vitta.me>
2021-07-26 22:44:58 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+};
