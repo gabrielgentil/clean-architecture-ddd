f733f569c7b8b60bad026d435639c6c52d723890
gabrielgentil <gabriel.sgentil97@gmail.com>
2021-09-03 14:25:46 -0300

    simplify validation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,14 @@
-import { HttpRequest, HttpResponse} from '../protocols/http'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { BadRequest } from '../helpers/http-helpers'
 
-
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return BadRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return BadRequest(new MissingParamError('email'))
+    const requiredField = ['name', 'email']
+    for (const field of requiredField) {
+      if (!httpRequest.body[field]) {
+        return BadRequest(new MissingParamError(field))
+      }
     }
   }
 }
