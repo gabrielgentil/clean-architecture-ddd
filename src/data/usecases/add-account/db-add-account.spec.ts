420bc35ac603174814ba9d601a81667fd97d559e
gabrielgentil <gabriel.sgentil97@gmail.com>
2021-09-13 15:20:53 -0300

    test: ensure DbAddAccount return an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -111,5 +111,20 @@ describe('DbAddAccount', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'hashed_password'
+    })
+  })
 })
