d38212353600fcb5211d62a813e38060570864e0
gabrielgentil <gabriel.sgentil97@gmail.com>
2021-09-22 18:36:15 -0300

    ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as json ', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app).post('/test_body_parser').send({name: 'Gabriel'}).expect({name: 'Gabriel'})
+  })
+})
